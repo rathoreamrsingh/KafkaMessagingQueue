b983509ebfa14f0a538a6da8cea2978ac9b42ce6
Amar Singh <amarsingh@outlook.in>
2018-03-22 23:43:31 +0530

    first commit

---
+++ KafkaProducer/src/main/java/KafkaProducer/KafkaProducer/App.java
@@ -0,0 +1,13 @@
+package KafkaProducer.KafkaProducer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
